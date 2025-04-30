8064b303b613618d63cac78ef7a909b23cf3794c
joyeongchan <tigerbone@naver.com>
2025-04-30 15:22:01 +0900

    고양이 사진 게시판 select 추가, 디버깅

--- board_app/models.py
+++ board_app/models.py
@@ -7,10 +7,13 @@ class Post(models.Model):
     content = models.TextField(verbose_name='내용')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='작성자')
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='posts', verbose_name='반려동물')
+    pets = models.ManyToManyField(Pet, related_name='together_posts', verbose_name='함께한 반려동물들', blank=True)
     image = models.ImageField(upload_to='post_images/', verbose_name='이미지')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성일')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True, verbose_name='좋아요')
+    is_together = models.BooleanField(default=False, verbose_name='함께 작성')
+    is_etc = models.BooleanField(default=False, verbose_name='기타 작성')
 
     class Meta:
         ordering = ['-created_at']
--- board_app/urls.py
+++ board_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:post_id>/edit/', views.post_edit, name='edit'),
     path('<int:post_id>/delete/', views.post_delete, name='delete'),
     path('<int:post_id>/comment/', views.comment_create, name='comment_create'),
+    path('<int:post_id>/comment/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),
     path('<int:post_id>/like/', views.post_like, name='like'),
 ] 
