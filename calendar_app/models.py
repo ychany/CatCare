f160e7a1be8edcd6c2ea93b44cb19e5cbf6729da
joyeongchan <tigerbone@naver.com>
2025-05-11 02:32:48 +0900

    건강 캘린더 동물병원 필드추가 (카카오맵)

--- calendar_app/models.py
+++ calendar_app/models.py
@@ -13,6 +13,7 @@ class Event(models.Model):
     description = models.TextField(blank=True)
     notes = models.TextField(blank=True)
     next_date = models.DateField(blank=True, null=True)
+    hospital = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
